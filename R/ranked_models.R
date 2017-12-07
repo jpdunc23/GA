1c6108a3b6a4d1d866a037aac9b6147ead36e396
ffandong <31294915+ffandong@users.noreply.github.com>
2017-12-06 17:20:12 -0800

    add AIC lookup

--- R/ranked_models.R
+++ R/ranked_models.R
@@ -5,6 +5,17 @@
 # So for this moment, I got rid of the "error_func" argument and replaced 
 #   error_func(model) with AIC(model).
 # Hopefully we can fix the bug together.
+
+X <- mtcars[-1] 
+y <- unlist(mtcars[1])
+index <-initialize_parents(10,20)$index
+AIC <- lapply(index, calculate_aic, X, y, AIC)
+model_AIC <- data.frame(sapply(list(index), `[`))
+colnames(model_AIC) <- c('Index')
+model_AIC$AIC <- unlist(AIC)
+model_AIC <- arrange(model_AIC,AIC)
+
+ranked_models(index, X, y)
 #################################################################################
 
 
@@ -51,7 +62,7 @@ initialize_parents <- function(feature_count, generation_count=100) {
   return (list("binary" = binary_list, "index" = index_list))
 }
 
-calculate_aic <- function(index, X, y) {
+calculate_fitness <- function(index, X, y, error_func) {
   ## inputs:
   ##   X               Data frame of selected features
   ##   y               Output variable
@@ -62,41 +73,78 @@ calculate_aic <- function(index, X, y) {
   ##
   ## Examples:
   ##  aic <- calculate_aic(data.frame(replicate(10,sample(0:1,1000,rep=TRUE))), 1:1000)
+  index.str <- deparse(index)
+  if (index.str %in% names(dict.fitness)) {
+    return(get(index.str, envir = dict.fitness))
+  } else{
+    X <- X[,index]
+    X <- as.data.frame(X)
+    y <- as.vector(y)
+    
+    is_data.frame(X)
+    is_vector(y)
+    
+    stopifnot(nrow(X) == length(y))
+    
+    X$y <- y
+    
+    model <- lm(y ~ ., data = X)
+    fitness <- error_func(model)
+    assign(index.str, fitness, envir = dict.fitness)
+    return (fitness) 
+  }
   
-  X <- X[,index]
-  X <- as.data.frame(X)
-  y <- as.vector(y)
-  
-  is_data.frame(X)
-  is_vector(y)
-  
-  stopifnot(nrow(X) == length(y))
-  
-  X$y <- y
-  
-  model <- lm(y ~ ., data = X)
-  return (AIC(model)) 
 }
 
-ranked_models <- function(index, X, y) {
-  ## inputs:
-  ##   chromosome      Output of initialize_parents()
-  ##   X               Data frame of all features
-  ##   y               Output variable
-  ##
-  ## output: a data frame containing binary list, index list and their
-  ##          respective AIC, sorted by AIC in ascending order
-  ## 
-  ## The smaller the AIC, the better the fit.
-  ## 
-  ## Examples:
-  ##  chromosome <- initialize_parents(5)
-  ##  ranked_models(chromosome, df, y)
+ranked_models <- function(index, X, y, error_func=AIC) {
+  #' Fit the models and rank them by their fitness function.
+  #'
+  #' @param index A list of indices of selected variables. 
+  #' @param X Data frame of all features
+  #' @param y Dependent variable
+  #' @param error_func Error function for fitness measurement. Default is AIC.
+  #' @return a data frame containing index list and their respective AIC, sorted by AIC in ascending order
+  #' @examples
+  #' X <- mtcars[-1] 
+  #' y <- unlist(mtcars[1])
+  #' index <-initialize_parents(10,20)$index
+  #' ranked_models(index, X, y)
+  #' 
 
-  AIC <- lapply(index, calculate_aic, X, y)
-  model_AIC <- data.frame(sapply(list(index), `[`))
-  colnames(model_AIC) <- c('Index')
-  model_AIC$AIC <- unlist(AIC)
-  model_AIC <- arrange(model_AIC,AIC)
-  return(model_AIC)
+  fitness <- lapply(index, calculate_fitness, X, y, error_func)
+  model_fitness <- data.frame(sapply(list(index), `[`))
+  colnames(model_fitness) <- c('Index')
+  model_fitness$fitness <- unlist(fitness)
+  model_fitness <- arrange(model_fitness,fitness)
+  return(model_fitness)
 }
+
+
+# parent_str <- deparse(parent)
+# memo_AIC <- new.env()
+# 
+# deparse(c(1,2,3))
+# 
+# s <- deparse(c(1,2,3))
+# 
+# memo_AIC$test <- 1
+# memo_AIC$test
+# 
+# memo_AIC$`s`
+# memo_AIC
+# 
+# names(memo_AIC)
+# 
+# ls(memo_AIC)
+# get(s, memo_AIC)
+# 
+# parent <- c(1,2,3)
+# parent_str <- deparse(parent)
+# parent_str
+# 
+# assign(parent_str, 497.1, envir = dict.AIC)
+# ls(memo_AIC)
+# get(parent_str, envir = dict.AIC)
+# 
+# "c(2,3,4)" %in% names(memo_AIC)
+# 

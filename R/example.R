b945edf69b63416c434f7bc655c752fa2704cb49
Kunal Desai <kunaljaydesai@gmail.com>
2017-12-08 15:24:59 -0800

    examples cleaned

--- R/example.R
+++ R/example.R
@@ -7,9 +7,9 @@ beta <- c(88, 0.1, 123, 4563, 1.23, 20)
 y <- X[ ,1:6] %*% beta
 colnames(X) <- c(paste("real", 1:6, sep = ""),
                  paste("noi", 1:34, sep = ""))
-system.time(
-     o1 <- select(X, y, nsplits = 3, max_iter = 200)
-)
+# system.time(
+#      o1 <- select(X, y, nsplits = 3, max_iter = 200)
+# )
 # o1
 # system.time(
 #     o2 <- select(X, y, selection = "proportional", n_splits = 3)

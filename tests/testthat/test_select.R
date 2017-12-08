110f7e3c7e05670da0821d847affb0eadf4f18a0
Kunal Desai <kunaljaydesai@gmail.com>
2017-12-08 15:28:33 -0800

    examples cleaned

--- tests/testthat/test_select.R
+++ tests/testthat/test_select.R
@@ -1,3 +1,3 @@
 test_that("Basic test cases", {
-  expect_that(next_gen <- select(mtcars[-1], unlist(mtcars[1]), randomness=TRUE, k=5, generation_count=100, error_func=BIC), is_a("data.frame"))
+  expect_that(next_gen <- select(mtcars[-1], unlist(mtcars[1]), randomness=TRUE, k=5, generation_count=100, error_func=BIC), is_a("list"))
 })

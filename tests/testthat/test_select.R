7dbdcca579797cc5545e10b176a70882c2e57c02
James Duncan <jpduncan@berkeley.edu>
2017-12-14 12:19:24 -0800

    Add some more tests of select

--- tests/testthat/test_select.R
+++ tests/testthat/test_select.R
@@ -1,5 +1,15 @@
 context("select tests")
 
-test_that("Basic test cases", {
-  expect_that(next_gen <- select(mtcars[-1], unlist(mtcars[1]), randomness=TRUE, k=5, generation_count=100, error_func=BIC), is_a("list"))
+X <- mtcars[-1]
+y <- mtcars[, 1]
+
+test_that("Test for correct output of select with various inputs", {
+  expect_true(is.list(select(X, y, max_iter = 30)))
+  expect_true(is.list(select(X, y, family = poisson, max_iter = 30)))
+  expect_true(is.list(select(X, y, K = 5, max_iter = 30)))  
+  expect_true(is.list(select(X, y, selection = "proportional", max_iter = 30)))
+  expect_true(is.list(select(X, y, selection = "proportional", randomness = FALSE,
+                             max_iter = 30)))
+  expect_true(is.list(select(X, y, fit_func = BIC, max_iter = 30)))
+  expect_true(is.list(select(X, y, parallel = FALSE, max_iter = 30)))
 })
